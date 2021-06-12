3c5de3514e065f97fcf53ffcbe3b5a144c469db3
Maris Basha <maris.basha1@gmail.com>
2021-06-12 23:55:04 +0200

    d

---
+++ data/sh/script.sh
@@ -0,0 +1,14 @@
+exec > ./data/logs/output.txt 2>&1
+for n in $(cat ./data/config)
+do
+   ./seq 41 41 $(echo $n | cut -d ":" -f 1) $(echo $n | cut -d ":" -f 2) $(echo $n | cut -d ":" -f 3)
+   for w in 1 2 4 8 16 32 64 128 256
+   do
+      for s in {1..10}
+      do
+         ./par 41 41 ${w} $(echo $n | cut -d ":" -f 1) $(echo $n | cut -d ":" -f 2) $(echo $n | cut -d ":" -f 3)
+         ./ff 41 41 ${w} $(echo $n | cut -d ":" -f 1) $(echo $n | cut -d ":" -f 2) $(echo $n | cut -d ":" -f 3)
+         ./ff 41 41 ${w} $(echo $n | cut -d ":" -f 1) $(echo $n | cut -d ":" -f 2) $(echo $n | cut -d ":" -f 3) farm
+      done
+   done
+done
